81ff6e74794b70828d41c8e423eafde0fc733957
wastegas <wastegas@gmail.com>
2020-09-08 08:42:44 -1000

    new matrix translation

---
+++ mat_trans/main.cpp
@@ -0,0 +1,110 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <cassert>
+#include "gl_utils.h"
+#include "logging.h"
+
+int g_gl_width = 640;
+int g_gl_height = 480;
+GLFWwindow* g_window;
+
+static void _update_fps_counter(GLFWwindow* window) {
+  static double previous_seconds = glfwGetTime();
+  static int frame_count;
+  double current_seconds = glfwGetTime();
+  double elapse_seconds = current_seconds - previous_seconds;
+  if (elapse_seconds > 0.25) {
+    previous_seconds = current_seconds;
+    double fps = (double)frame_count / elapse_seconds;
+    char tmp[128];
+    sprintf(tmp, "opengl @ fps: %.2f", fps);
+    glfwSetWindowTitle(window, tmp);
+    frame_count = 0;
+  }
+  frame_count++;
+}
+
+int main()
+{
+  assert(restart_gl_log());
+  // starg GL context and O/S window using GLFW helper library
+  gl_log("starting GLFW\n%s\n", glfwGetVersionString());
+  // register the error call-back function we wrote
+  start_gl();
+
+  // tell GL to only draw onto a pixel if the shape is closer to the viewer
+  glEnable (GL_DEPTH_TEST); // enable depth-testing
+  glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+
+  /* other stuff goes here */
+  GLfloat points[] = {
+		      0.0f, 0.5f, 0.0f, // top point
+		      0.5f, -0.5f, 0.0f, // bot right point
+		      -0.5f, -0.5f, 0.0f  // bot left point
+  };
+
+  GLuint vbo = 0; // our vertex buffer
+  glGenBuffers (1, &vbo); // set as the current buffer
+  glBindBuffer (GL_ARRAY_BUFFER, vbo);
+  // copy our points into the currently bound buffer
+  glBufferData (GL_ARRAY_BUFFER, sizeof (points), points, GL_STATIC_DRAW);
+
+  GLuint vao = 0; // our mesh aka vertext array
+  glGenVertexArrays (1, &vao); // turn vao into a mesh
+  glBindVertexArray(vao); // make it current mesh
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo); // put our buffer into the mesh
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+  //Shader programs
+  const char* vertex_shader =
+    "#version 400\n"
+    "in vec3 vp;"
+    "void main() {"
+    "  gl_Position = vec4 (vp, 1.0);"
+    "}";
+
+  const char* fragment_shader =
+    "#version 400\n"
+    "out vec4 frag_colour;"
+    "void main() {"
+    "  frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+    "}";
+
+  GLuint vs = glCreateShader (GL_VERTEX_SHADER);
+  glShaderSource (vs, 1, &vertex_shader, NULL);
+  glCompileShader(vs);
+  GLuint fs = glCreateShader (GL_FRAGMENT_SHADER);
+  glShaderSource (fs, 1, &fragment_shader, NULL);
+  glCompileShader(fs);
+
+  GLuint shader_programme = glCreateProgram();
+  glAttachShader (shader_programme, fs);
+  glAttachShader (shader_programme, vs);
+  glLinkProgram (shader_programme);
+
+  // draw our triangle
+  while(!glfwWindowShouldClose (g_window)) {
+    _update_fps_counter(g_window);
+      //wipe the drawing surface clear
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      glViewport(0, 0, g_gl_width, g_gl_height);
+      glUseProgram(shader_programme);
+      glBindVertexArray(vao);
+      //draw points 0-3 from the currently bound VAO with current in-use shader
+      glDrawArrays(GL_TRIANGLES, 0, 3);
+      //update other events like input handling
+      glfwPollEvents();
+      if (GLFW_PRESS == glfwGetKey(g_window, GLFW_KEY_ESCAPE))
+	{
+	  glfwSetWindowShouldClose(g_window, 1);
+	}
+      //put the stuff we've been drawing onto the display
+      glfwSwapBuffers (g_window);
+    }
+  
+  // close GL context and any other GLFW resources
+  glfwTerminate();
+  return 0;
+}
