6fcef79462a37265b3e5f0f3ad21ae7ecf7e7d2a
wastegas <wastegas@gmail.com>
2020-09-14 13:10:07 -1000

    update shader

--- virt_cam/test5_vs.glsl
+++ virt_cam/test5_vs.glsl
@@ -1,7 +1,7 @@
 #version 400
 
-in vec3 vertex_position;
-in vec3 vertex_colour;
+layout(location = 0) in vec3 vertex_position;
+layout(location = 1) in vec3 vertex_colour;
 
 uniform mat4 view, proj;
 
